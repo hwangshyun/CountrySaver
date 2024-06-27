684f1751d74afbdbb7ccbfab30ea8316dc036043
hwangseunghyun <hwamg123@naver.com>
2024-06-28 03:18:33 +0900

    오류수정

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,14 +1,10 @@
-// src/api/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Key:', supabaseKey);
-
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase URL and Key must be defined in .env file');
+  throw new Error("수파베이스오류");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
